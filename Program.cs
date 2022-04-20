79bb4a41f4273affc2faa8037fdff322545b8ccc
rkcbalanza <richard.balanza@pointwest.com.ph>
2022-04-20 11:11:54 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myFirstRepo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
